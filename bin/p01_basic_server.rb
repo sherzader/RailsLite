4a7abf6d69922e57d0d816e27fd5a0c0532a74ea
App Academy Student <student@appacademy.io>
2015-11-24 14:58:14 -0800

    full path writes to browser

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -15,11 +15,27 @@ class App
   end
 end
 
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  res.write(req.fullpath)
+  #res.write("Hello Cats")
+  res.finish
+end
 # 5. pass object App.new to Rack::Server#start
 # can also pass Proc:
-# Proc.new { |env| ['200'. {'Content-Type' => 'text/html'}. [''] ] }
+# app = Proc.new { |env| ['200'. {'Content-Type' => 'text/html'}. [''] ] }
 Rack::Server.start(
-  app: App.new,
+  app: app,
   #port defaults to 8080
   Port: 3000
 )
+
+# app = Proc.new do |env|
+#   req = Rack::Request.new(env)
+#   res = Rack::Response.new #serializes/formats env object
+#   #MyController is like ApplicationsController
+#   MyController.new(req, res).go #go method build response object
+#   res.finish
+# end
